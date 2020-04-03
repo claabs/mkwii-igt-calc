b7c0def7395ffe7b9e41a8573f835f6806bce71f
Charlie Laabs <charlielaabs@gmail.com>
2020-04-03 17:56:58 -0500

    Improve production build method (Rollup)

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,25 @@
-/**
- * @license
- * Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at
- * http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at
- * http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at
- * http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at
- * http://polymer.github.io/PATENTS.txt
- */
-
+import { createDefaultConfig } from '@open-wc/building-rollup';
+import cpy from 'rollup-plugin-cpy';
 import filesize from 'rollup-plugin-filesize';
-import { terser } from 'rollup-plugin-terser';
-import resolve from 'rollup-plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
 
-export default {
-  input: 'dist/mkwii-igt-calc-app.js',
+const config = createDefaultConfig({
+  input: './index.html',
+});
+
+export default // add plugin to the first config
+{
+  ...config,
   output: {
-    file: 'dist/mkwii-igt-calc-app.bundled.js',
-    format: 'esm',
-  },
-  onwarn(warning) {
-    if (warning.code !== 'THIS_IS_UNDEFINED') {
-      console.error(`(!) ${warning.message}`);
-    }
+    ...config.output,
+    sourcemap: false,
   },
   plugins: [
-    replace({ 'Reflect.decorate': 'undefined' }),
-    resolve(),
-    terser({
-      module: true,
-      warnings: true,
-      mangle: {
-        properties: {
-          regex: /^__/,
-        },
-      },
+    ...config.plugins,
+    cpy({
+      // copy over all images files
+      files: ['favicon.ico', 'manifest.json'],
+      dest: 'dist',
     }),
-    filesize({
-      showBrotliSize: true,
-    }),
+    filesize(),
   ],
 };
