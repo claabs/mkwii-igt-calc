b2e4beca6ee564fb466840f0d01cc1c722c6ef46
Charlie Laabs <charlielaabs@gmail.com>
2020-04-04 22:59:00 -0500

    Hopefully working service-worker

--- workbox-config.js
+++ workbox-config.js
@@ -2,10 +2,47 @@
 const path = require('path');
 
 module.exports = {
+  swDest: path.join(__dirname, 'dist', 'sw.js'),
   globDirectory: path.join(__dirname, 'dist'),
   globPatterns: ['**/*.{js,png,ico,svg,html,json,css}'],
-  swDest: path.join(__dirname, 'dist', 'sw.js'),
   modifyUrlPrefix: {
     '/': '',
   },
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'google-fonts-stylesheets',
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts',
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/ajax\.googleapis\.com/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'typekit-loader',
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        },
+      },
+    },
+  ],
 };
